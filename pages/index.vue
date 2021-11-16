044e9ca7861f91b69c6647c26e77155529903215
Zoopen <zt947313341@qq.com>
2021-11-16 23:04:47 +0800

    touch tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Tutorial />
+    <p class="focus:ring-2 focus:ring-red-500">hello word</p>
+    <button class="btn-indigo">Click me</button>
+    <button class="btn-blue">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.btn-indigo {
+  @apply py-2 px-4 bg-indigo-500 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-75;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1200px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
